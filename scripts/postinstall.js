06a9eb96b4caa5d744b7b7f19f6fdc744e559b05
Tetsuya Mori <monry84@gmail.com>
2018-02-26 17:00:10 +0900

    :sparkles: Improve install script
    * 移動するファイル・ディレクトリを配列で定義
    * Android Library Project としてビルドするためのファイルを移動

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -27,6 +27,20 @@ if (/^@/.test(package.name)) {
   package_name = package.name;
 }
 
+// umm インストール後に移動させるファイル群
+var move_items = [
+  '/Plugins/Android/Firebase/AndroidManifest.xml',
+  '/Plugins/Android/Firebase/AndroidManifest.xml.meta',
+  '/Plugins/Android/Firebase/project.properties',
+  '/Plugins/Android/Firebase/project.properties.meta',
+  '/Firebase/Editor/generate_xml_from_google_services_json.exe',
+  '/Firebase/Editor/generate_xml_from_google_services_json.exe.meta',
+  '/Firebase/Editor/generate_xml_from_google_services_json.py',
+  '/Firebase/Editor/generate_xml_from_google_services_json.py.meta',
+  '/Editor Default Resources/Firebase/',
+  '/Editor Default Resources/Firebase.meta',
+];
+
 // スクリプトの存在するディレクトリから見たパス
 sync(
   path.resolve(script_directory, '../Assets/'),
@@ -37,72 +51,21 @@ sync(
       console.log(err);
       process.exit(1);
     }
-    sync(
-      path.resolve(script_directory, (has_scope ? '../' : '') + '../../../Assets/Modules/' + package_name + '/Firebase/Editor/generate_xml_from_google_services_json.exe'),
-      path.resolve(script_directory, (has_scope ? '../' : '') + '../../../Assets/Firebase/Editor/generate_xml_from_google_services_json.exe'),
-      { remove_source: true },
-      function(err) {
-        if (err) {
-          console.log(err);
-          process.exit(1);
-        }
+    move_items.forEach(
+      function(move_item) {
+        sync(
+          path.resolve(script_directory, (has_scope ? '../' : '') + '../../../Assets/Modules/' + package_name + move_item),
+          path.resolve(script_directory, (has_scope ? '../' : '') + '../../../Assets' + move_item),
+          { remove_source: true },
+          function(err) {
+            if (err) {
+              console.log(err);
+              process.exit(1);
+            }
+          }
+        );
       }
-    );
-    sync(
-      path.resolve(script_directory, (has_scope ? '../' : '') + '../../../Assets/Modules/' + package_name + '/Firebase/Editor/generate_xml_from_google_services_json.exe.meta'),
-      path.resolve(script_directory, (has_scope ? '../' : '') + '../../../Assets/Firebase/Editor/generate_xml_from_google_services_json.exe.meta'),
-      { remove_source: true },
-      function(err) {
-        if (err) {
-          console.log(err);
-          process.exit(1);
-        }
-      }
-    );
-    sync(
-      path.resolve(script_directory, (has_scope ? '../' : '') + '../../../Assets/Modules/' + package_name + '/Firebase/Editor/generate_xml_from_google_services_json.py'),
-      path.resolve(script_directory, (has_scope ? '../' : '') + '../../../Assets/Firebase/Editor/generate_xml_from_google_services_json.py'),
-      { remove_source: true },
-      function(err) {
-        if (err) {
-          console.log(err);
-          process.exit(1);
-        }
-      }
-    );
-    sync(
-      path.resolve(script_directory, (has_scope ? '../' : '') + '../../../Assets/Modules/' + package_name + '/Firebase/Editor/generate_xml_from_google_services_json.py.meta'),
-      path.resolve(script_directory, (has_scope ? '../' : '') + '../../../Assets/Firebase/Editor/generate_xml_from_google_services_json.py.meta'),
-      { remove_source: true },
-      function(err) {
-        if (err) {
-          console.log(err);
-          process.exit(1);
-        }
-      }
-    );
-    sync(
-      path.resolve(script_directory, (has_scope ? '../' : '') + '../../../Assets/Modules/' + package_name + '/Editor Default Resources/Firebase/'),
-      path.resolve(script_directory, (has_scope ? '../' : '') + '../../../Assets/Editor Default Resources/Firebase/'),
-      { remove_source: true },
-      function(err) {
-        if (err) {
-          console.log(err);
-          process.exit(1);
-        }
-      }
-    );
-    sync(
-      path.resolve(script_directory, (has_scope ? '../' : '') + '../../../Assets/Modules/' + package_name + '/Editor Default Resources/Firebase.meta'),
-      path.resolve(script_directory, (has_scope ? '../' : '') + '../../../Assets/Editor Default Resources/Firebase.meta'),
-      { remove_source: true },
-      function(err) {
-        if (err) {
-          console.log(err);
-          process.exit(1);
-        }
-      }
-    );
+    )
   }
 );
 
